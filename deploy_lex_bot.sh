a8470dac1644b9fbfd19f4f62d9abdeda14cfe9c
zane <zane.williamson@gmail.com>
2017-08-14 22:16:00 -0700

    adding lambda policy updates

--- deploy_lex_bot.sh
+++ deploy_lex_bot.sh
@@ -27,6 +27,65 @@ function swap_in_account_number {
   sed -i -e "s/REPLACE_ME_ACCT/$AWSACCT/g" _build_lex/*.json
 }
 
+function add_lambda_policies {
+  echo "Adding lambda policy 2 for set_office_hours..."
+  aws lambda add-permission \
+    --region us-east-1 \
+    --function-name set_office_hours \
+    --statement-id 2 \
+    --principal lex.amazonaws.com \
+    --action lambda:InvokeFunction \
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:* 
+  echo "Adding lambda policy 3 for set_office_hours..."
+  aws lambda add-permission \
+    --region us-east-1 \
+    --function-name set_office_hours \
+    --statement-id 3 \
+    --principal lex.amazonaws.com \
+    --action lambda:InvokeFunction \
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:SetTodaysOfficeHours:* 
+  echo "Adding lambda policy 1 for add_discussion_topic..."
+  aws lambda add-permission \
+    --region us-east-1 \
+    --function-name add_discussion_topic \
+    --statement-id 1 \
+    --principal lex.amazonaws.com \
+    --action lambda:InvokeFunction \
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:* 
+  echo "Adding lambda policy 1 for get_office_hours..."
+  aws lambda add-permission \
+    --region us-east-1 \
+    --function-name get_office_hours \
+    --statement-id 1 \
+    --principal lex.amazonaws.com \
+    --action lambda:InvokeFunction \
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:* 
+  echo "Adding lambda policy 2 for get_office_hours..."
+  aws lambda add-permission \
+    --region us-east-1 \
+    --function-name get_office_hours \
+    --statement-id 2 \
+    --principal lex.amazonaws.com \
+    --action lambda:InvokeFunction \
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:GetTeamsOfficeHours:* 
+  echo "Adding lambda policy 1 for lookup_discussion_topics..."
+  aws lambda add-permission \
+    --region us-east-1 \
+    --function-name lookup_discussion_topics \
+    --statement-id 1 \
+    --principal lex.amazonaws.com \
+    --action lambda:InvokeFunction \
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:* 
+  echo "Adding lambda policy 2 for lookup_discussion_topics..."
+  aws lambda add-permission \
+    --region us-east-1 \
+    --function-name lookup_discussion_topics \
+    --statement-id 2 \
+    --principal lex.amazonaws.com \
+    --action lambda:InvokeFunction \
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:LookupDiscussionTopics:* 
+}
+
 function put_slots {
   echo "Deploying slot GetDiscussionTopic..."
   aws lex-models put-slot-type \
@@ -62,10 +121,11 @@ function put_intents {
 }
 
 function main {
-  prep_build
-  swap_in_account_number
-  put_slots
-  put_intents
+  add_lambda_policies
+  #prep_build
+  #swap_in_account_number
+  #put_slots
+  #put_intents
 }
 
 main
