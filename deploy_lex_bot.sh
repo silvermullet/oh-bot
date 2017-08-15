eaacc39f285f8c372acf2ffcb6a44edfbb728f68
zane <zane.williamson@gmail.com>
2017-08-14 20:48:37 -0700

    updating deploy

--- deploy_lex_bot.sh
+++ deploy_lex_bot.sh
@@ -23,41 +23,42 @@ function prep_build {
 }
 
 function swap_in_account_number {
-  echo "Swap in account number provided by -a flag..."
+  echo "Using account number: $AWSACCT"
   sed -i -e "s/REPLACE_ME_ACCT/$AWSACCT/g" _build_lex/*.json
 }
 
 function put_slots {
+  echo "Deploying slot GetDiscussionTopic..."
   aws lex-models put-slot-type \
     --name GetDiscussionTopic \
     --cli-input-json file://_build_lex/put-slot-type-GetDiscussionTopic.json
+  echo "Deploying slot OfficeHoursLocation..."
   aws lex-models put-slot-type \
     --name OfficeHoursLocation \
     --cli-input-json file://_build_lex/put-slot-type-OfficeHoursLocation.json
+  echo "Deploying slot SetTeam..."
   aws lex-models put-slot-type \
     --name SetTeam \
     --cli-input-json file://_build_lex/put-slot-type-SetTeam.json
 }
 
 function put_intents {
+  echo "Deploying intent AddDiscussionTopicOfficeHours..."
   aws lex-models put-intent \
     --name AddDiscusionTopicOfficeHours \
     --cli-input-json file://_build_lex/put-intent-AddDiscussionTopicOfficeHours.json
+  echo "Deploying intent GetTeamsOfficeHours..."
   aws lex-models put-intent \
     --name GetTeamsOfficeHours \
     --cli-input-json file://_build_lex/put-intent-GetTeamsOfficeHours.json
+  echo "Deploying intent LookUpDiscussionTopic..."
   aws lex-models put-intent \
-    --name LookUpDiscussionTopic \
-    --cli-input-json file://_build_lex/put-intent-LookUpDiscussionTopic.json
+    --name LookupDiscussionTopics \
+    --cli-input-json file://_build_lex/put-intent-LookupDiscussionTopic.json
+  echo "Deploying intent SetTodaysOfficeHours..."
   aws lex-models put-intent \
     --name SetTodaysOfficeHours \
     --cli-input-json file://_build_lex/put-intent-SetTodaysOfficeHours.json
-  aws lex-models put-intent \
-    --name GetDiscussionTopic \
-    --cli-input-json file://_build_lex/put-slot-type-GetDiscussionTopic.json
-  aws lex-models put-intent \
-    --name OfficeHoursLocation \
-    --cli-input-json file://_build_lex/put-slot-type-OfficeHoursLocation.json
 }
 
 function main {
