390aa07627ca04d4c1e9543848f9c4c1702e8fa4
Zane Williamson <zanew@zillow.com>
2017-08-16 12:09:09 -0700

    Updating deployment json

--- deploy_lex_bot.sh
+++ deploy_lex_bot.sh
@@ -35,7 +35,7 @@ function add_lambda_policies {
     --statement-id 2 \
     --principal lex.amazonaws.com \
     --action lambda:InvokeFunction \
-    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:* 
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:*
   echo "Adding lambda policy 3 for set_office_hours..."
   aws lambda add-permission \
     --region us-east-1 \
@@ -43,7 +43,7 @@ function add_lambda_policies {
     --statement-id 3 \
     --principal lex.amazonaws.com \
     --action lambda:InvokeFunction \
-    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:SetTodaysOfficeHours:* 
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:SetTodaysOfficeHours:*
   echo "Adding lambda policy 1 for add_discussion_topic..."
   aws lambda add-permission \
     --region us-east-1 \
@@ -51,7 +51,7 @@ function add_lambda_policies {
     --statement-id 1 \
     --principal lex.amazonaws.com \
     --action lambda:InvokeFunction \
-    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:* 
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:*
   echo "Adding lambda policy 1 for get_office_hours..."
   aws lambda add-permission \
     --region us-east-1 \
@@ -59,7 +59,7 @@ function add_lambda_policies {
     --statement-id 1 \
     --principal lex.amazonaws.com \
     --action lambda:InvokeFunction \
-    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:* 
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:*
   echo "Adding lambda policy 2 for get_office_hours..."
   aws lambda add-permission \
     --region us-east-1 \
@@ -67,7 +67,7 @@ function add_lambda_policies {
     --statement-id 2 \
     --principal lex.amazonaws.com \
     --action lambda:InvokeFunction \
-    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:GetTeamsOfficeHours:* 
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:GetTeamsOfficeHours:*
   echo "Adding lambda policy 1 for lookup_discussion_topics..."
   aws lambda add-permission \
     --region us-east-1 \
@@ -75,7 +75,7 @@ function add_lambda_policies {
     --statement-id 1 \
     --principal lex.amazonaws.com \
     --action lambda:InvokeFunction \
-    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:* 
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:AddDiscusionTopicOfficeHours:*
   echo "Adding lambda policy 2 for lookup_discussion_topics..."
   aws lambda add-permission \
     --region us-east-1 \
@@ -83,49 +83,72 @@ function add_lambda_policies {
     --statement-id 2 \
     --principal lex.amazonaws.com \
     --action lambda:InvokeFunction \
-    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:LookupDiscussionTopics:* 
+    --source-arn arn:aws:lex:us-east-1:${AWSACCT}:intent:LookupDiscussionTopics:*
 }
 
 function put_slots {
   echo "Deploying slot GetDiscussionTopic..."
   aws lex-models put-slot-type \
     --name GetDiscussionTopic \
-    --cli-input-json file://_build_lex/put-slot-type-GetDiscussionTopic.json
+    --cli-input-json file://_build_lex/put-slot-type-GetDiscussionTopic.json \
+    --region us-east-1 \
+    --checksum ""
   echo "Deploying slot OfficeHoursLocation..."
   aws lex-models put-slot-type \
     --name OfficeHoursLocation \
-    --cli-input-json file://_build_lex/put-slot-type-OfficeHoursLocation.json
+    --cli-input-json file://_build_lex/put-slot-type-OfficeHoursLocation.json \
+    --region us-east-1 \
+    --checksum ""
   echo "Deploying slot SetTeam..."
   aws lex-models put-slot-type \
     --name SetTeam \
-    --cli-input-json file://_build_lex/put-slot-type-SetTeam.json
+    --cli-input-json file://_build_lex/put-slot-type-SetTeam.json \
+    --region us-east-1 \
+    --checksum ""
 }
 
 function put_intents {
   echo "Deploying intent AddDiscussionTopicOfficeHours..."
   aws lex-models put-intent \
     --name AddDiscusionTopicOfficeHours \
-    --cli-input-json file://_build_lex/put-intent-AddDiscussionTopicOfficeHours.json
+    --cli-input-json file://_build_lex/put-intent-AddDiscussionTopicOfficeHours.json \
+    --region us-east-1 \
+    --checksum ""
   echo "Deploying intent GetTeamsOfficeHours..."
   aws lex-models put-intent \
     --name GetTeamsOfficeHours \
-    --cli-input-json file://_build_lex/put-intent-GetTeamsOfficeHours.json
+    --cli-input-json file://_build_lex/put-intent-GetTeamsOfficeHours.json \
+    --region us-east-1 \
+    --checksum ""
   echo "Deploying intent LookUpDiscussionTopic..."
   aws lex-models put-intent \
     --name LookupDiscussionTopics \
-    --cli-input-json file://_build_lex/put-intent-LookupDiscussionTopic.json
+    --cli-input-json file://_build_lex/put-intent-LookupDiscussionTopic.json \
+    --region us-east-1 \
+    --checksum ""
   echo "Deploying intent SetTodaysOfficeHours..."
   aws lex-models put-intent \
     --name SetTodaysOfficeHours \
-    --cli-input-json file://_build_lex/put-intent-SetTodaysOfficeHours.json
+    --cli-input-json file://_build_lex/put-intent-SetTodaysOfficeHours.json \
+    --region us-east-1 \
+    --checksum ""
+}
+
+function put_bot {
+  aws lex-models put-bot \
+    --name OfficeHoursBot \
+    --cli-input-json file://_build_lex/put-bot.json \
+    --region us-east-1 \
+    --checksum ""
 }
 
 function main {
-  add_lambda_policies
-  #prep_build
-  #swap_in_account_number
-  #put_slots
-  #put_intents
+  #add_lambda_policies
+  prep_build
+  swap_in_account_number
+  put_slots
+  put_intents
+  put_bot
 }
 
 main
